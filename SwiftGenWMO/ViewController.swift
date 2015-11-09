bc59d764a4d370e29ca80b8c144f8330b7a01435
Ben Chatelain <ben@octop.ad>
2015-11-09 16:31:36 -0700

    Use named text color in ViewController

--- SwiftGenWMO/ViewController.swift
+++ SwiftGenWMO/ViewController.swift
@@ -10,16 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var exampleLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        exampleLabel.textColor = UIColor(named: .TextColor)
     }
-
 
 }
 
